4ad09ba3fefc1898466d90bed4bf2f8f8415d7f7
Jdexurbano <dexterurbano25@gmail.com>
2025-06-04 17:30:39 +0800

    Create Login Page

--- src/pages/Auth/LoginPage.tsx
+++ src/pages/Auth/LoginPage.tsx
@@ -1,5 +1,117 @@
+import axios from "axios";
+import type { Token } from "@/types/Auth/auth";
+import { URL } from "@/utils/constant";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [username, setUsername] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    const payload = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post<Token>(`${URL}token/`, payload, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        const access = response.data.access;
+        console.log(access);
+        localStorage.setItem("token", access);
+        setPassword("");
+        setUsername("");
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <section className="w-full h-screen px-5 flex justify-center items-center bg-secondary">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">Sign Up</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form id="loginForm" onSubmit={handleLogin}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="eg:johndoe"
+                  required
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setUsername(e.target.value)
+                  }
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPassword(e.target.value)
+                  }
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button
+            type="submit"
+            className="w-full cursor-pointer"
+            form="loginForm"
+          >
+            Login
+          </Button>
+        </CardFooter>
+      </Card>
+    </section>
+  );
 }
 
 export default LoginPage;

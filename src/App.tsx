be44a032dfc0a3274172ab224b834f159581707d
Jdexurbano <dexterurbano25@gmail.com>
2025-06-06 13:38:42 +0800

    Add route for the signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import MainLayout from "./shared/layouts/MainLayout";
 import LoginPage from "./pages/Auth/LoginPage";
+import SignupPage from "./pages/Auth/SignupPage";
 import MainPage from "./pages/Main/MainPage";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       {/* public routes */}
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }

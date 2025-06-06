fde228dfca38955fd186c9ccc08f7301bbb2e918
Jdexurbano <dexterurbano25@gmail.com>
2025-06-06 23:22:12 +0800

    Add the auth provider component and private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { Route, Routes } from "react-router";
 import MainLayout from "./shared/layouts/MainLayout";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import PublicRoutes from "./utils/PublicRoutes";
 import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 import MainPage from "./pages/Main/MainPage";
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index path="/" element={<MainPage />} />
-      </Route>
-
-      {/* public routes */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<MainLayout />}>
+            <Route index path="/" element={<MainPage />} />
+          </Route>
+        </Route>
+        {/* public routes */}
+        <Route element={<PublicRoutes />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
